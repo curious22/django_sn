eea1bc3348115b9752bf7c1bf5af7e0b59cb4149
Ihor Peresunko <marazmatic77@gmail.com>
2020-02-14 11:32:23 +0200

    Updated time format in the Post serializer; Started to implement tests cases for Post resource

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
 from .models import Post
 
+HUMAN_DATETIME_FORMAT = '%d-%m-%Y %H:%M:%S%Z'
+
 
 class PostSerializer(serializers.ModelSerializer):
+    created = serializers.DateTimeField(format=HUMAN_DATETIME_FORMAT)
+    last_update = serializers.DateTimeField(format=HUMAN_DATETIME_FORMAT)
+
     class Meta:
         model = Post
         fields = '__all__'
--- apps/post/tests.py
+++ apps/post/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from apps.authentication.tests import BaseTestClass
+from .models import Post
+from .serializers import PostSerializer
+
+
+class TestPostResource(BaseTestClass):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.endpoint = '/api/v1/posts/'
+
+    def setUp(self):
+        super().setUp()
+        self.post = Post.objects.create(
+            author=self.base_user,
+            title='Test title',
+            text='Lorem ipsum'
+        )
+
+    def test_get_posts_unauth(self):
+        anonimus = APIClient()
+        resp = anonimus.get(self.endpoint)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_posts_success(self):
+        resp = self.client.get(self.endpoint)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        expected = PostSerializer(
+            Post.objects.all(),
+            many=True).data
+        self.assertEqual(resp.json(), expected)
