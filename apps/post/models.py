daa68899e24f6db1e278cf8e079f49b13aea7ca9
Ihor Peresunko <marazmatic77@gmail.com>
2020-02-14 11:16:29 +0200

    Added the Post model and init migrations

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='posts')
+    title = models.CharField(max_length=50, unique=True)
+    text = models.TextField(max_length=5000)
+    created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title}'
