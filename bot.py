3dc09f43f85f5f6b4001b26a810508df169743e0
Ihor Peresunko <marazmatic77@gmail.com>
2020-02-16 23:54:49 +0200

    Added check api and get fake user to the bot

--- bot.py
+++ bot.py
@@ -2,7 +2,14 @@ import json
 import os
 import sys
 
+import requests
+from faker import Faker
+from requests import exceptions
+
 CONFIG_FILE = 'bot_config.json'
+BASE_URL = 'http://localhost:8000/api/v1'
+SESSION = requests.Session()
+FAKE = Faker()
 
 
 def get_config(file_name):
@@ -27,6 +34,30 @@ def validate_config(config):
             sys.exit(1)
 
 
+def check_api(url):
+    """Checks API availability"""
+    try:
+        resp = SESSION.get(url)
+        if resp.status_code != 200:
+            return False
+    except exceptions.RequestException as err:
+        print(f'API host has not available: {err}')
+        return False
+
+    return True
+
+
+def get_fake_user():
+    """Generate a fake user for signup"""
+    data = {
+        'email': FAKE.email(),
+        'password': 'pass',
+        'first_name': FAKE.first_name(),
+        'last_name': FAKE.last_name()
+    }
+    return data
+
+
 if __name__ == '__main__':
     if not os.path.exists(CONFIG_FILE):
         print('Config file does not exist. Create it before using a bot')
@@ -34,4 +65,4 @@ if __name__ == '__main__':
 
     config = get_config(CONFIG_FILE)
     validate_config(config)
-    print(config)
+    check_api(BASE_URL)
