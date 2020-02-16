c2aebbc0aae2fac487ec14347894a36a3e80c457
Ihor Peresunko <marazmatic77@gmail.com>
2020-02-17 00:27:20 +0200

    Added user sigup and login

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+from collections import namedtuple
 
 import requests
 from faker import Faker
@@ -10,6 +11,14 @@ CONFIG_FILE = 'bot_config.json'
 BASE_URL = 'http://localhost:8000/api/v1'
 SESSION = requests.Session()
 FAKE = Faker()
+User = namedtuple('User', 'email access')
+
+API_MAPPER = {
+    'sigup': f'{BASE_URL}/auth/registration/',
+    'login': f'{BASE_URL}/auth/login/',
+    'create_post': f'{BASE_URL}/posts/',
+    'like': f'{BASE_URL}/likes/'
+}
 
 
 def get_config(file_name):
@@ -58,6 +67,26 @@ def get_fake_user():
     return data
 
 
+def user_login(cred):
+    """Get access token to registered user"""
+    resp = SESSION.post(API_MAPPER['login'], cred)
+    if resp.status_code == 200:
+        print(f'User {cred["email"]} has logined')
+        return resp.json()['access']
+    return False
+
+
+def user_registration(user):
+    """Register user and login (get access token)"""""
+    resp = SESSION.post(API_MAPPER['sigup'], user)
+    if resp.status_code == 201:
+        print(f'User {user["email"]} has registered')
+        access = user_login(user)
+        return User(user['email'], access)
+
+    return False
+
+
 if __name__ == '__main__':
     if not os.path.exists(CONFIG_FILE):
         print('Config file does not exist. Create it before using a bot')
@@ -66,3 +95,12 @@ if __name__ == '__main__':
     config = get_config(CONFIG_FILE)
     validate_config(config)
     check_api(BASE_URL)
+    users = []
+
+    # create users
+    for _ in range(config['number_of_users']):
+        email = user_registration(get_fake_user())
+        if email:
+            users.append(email)
+
+    print(users)
